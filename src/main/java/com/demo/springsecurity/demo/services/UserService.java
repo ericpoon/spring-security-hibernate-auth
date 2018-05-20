dc3c22faa415bb6d209f925735d1edc8af1b1577
Eric Zhiqi Pan <ericpoon510@gmail.com>
2018-05-20 15:16:46 +0800

    Enable to encrypt password using bcrypt

--- src/main/java/com/demo/springsecurity/demo/services/UserService.java
+++ src/main/java/com/demo/springsecurity/demo/services/UserService.java
@@ -4,6 +4,8 @@ import com.demo.springsecurity.demo.dao.UserDao;
 import com.demo.springsecurity.demo.entity.Authority;
 import com.demo.springsecurity.demo.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,7 +26,10 @@ public class UserService {
     }
 
     public void createUser(String name, String password, String[] authorities) {
-        User user = new User(name, "{noop}" + password);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
+
+        User user = new User(name, "{bcrypt}" + encodedPassword);
         List<Authority> authorityList = new ArrayList<>();
         for (String authority : authorities) {
             authorityList.add(new Authority(user, authority));
